9d06af18747a51f4766fad77af26bd3940112ae2
yohan <eras.projet@gmail.com>
2020-05-01 19:43:50 +0200

    Add database and dao

--- app/src/main/java/com/cleanup/todoc/model/Task.java
+++ app/src/main/java/com/cleanup/todoc/model/Task.java
@@ -1,5 +1,9 @@
 package com.cleanup.todoc.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -10,15 +14,21 @@ import java.util.Comparator;
  *
  * @author Gaëtan HERFRAY
  */
+@Entity(foreignKeys = @ForeignKey(entity = Project.class,
+        parentColumns = "id",
+        childColumns = "projectId"), indices = {@Index(value = {"projectId"}, unique = true)})
+
 public class Task {
     /**
      * The unique identifier of the task
      */
+    @PrimaryKey(autoGenerate = true)
     private long id;
 
     /**
      * The unique identifier of the project associated to the task
      */
+
     private long projectId;
 
     /**
@@ -113,6 +123,10 @@ public class Task {
     private void setCreationTimestamp(long creationTimestamp) {
         this.creationTimestamp = creationTimestamp;
     }
+
+    public long getCreationTimestamp() { return creationTimestamp; }
+
+    public long getProjectId() { return projectId; }
 
     /**
      * Comparator to sort task from A to Z
