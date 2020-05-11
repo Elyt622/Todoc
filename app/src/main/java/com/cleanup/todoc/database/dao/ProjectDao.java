45e2235ab9ec9a8248d27d242b1606462b7aad56
yohan <eras.projet@gmail.com>
2020-05-11 12:11:33 +0200

    Add AAC with MVVM, Room and Livedata

---
+++ app/src/main/java/com/cleanup/todoc/database/dao/ProjectDao.java
@@ -0,0 +1,9 @@
+package com.cleanup.todoc.database.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+
+@Dao
+public interface UserDao {
+    
+}
---
+++ app/src/main/java/com/cleanup/todoc/ui/TaskViewModel.java
@@ -0,0 +1,6 @@
+package com.cleanup.todoc.viewmodel;
+
+public class TaskViewModel {
+
+    
+}
